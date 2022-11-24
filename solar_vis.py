b4f811c7d518e2ccf714332e9159b40bc5733fe4
JopKingSinnn <djebancom504@gmail.com>
2022-11-24 11:49:22 +0300

    Все пофиксил

--- solar_vis.py
+++ solar_vis.py
@@ -1,4 +1,5 @@
 gravitational_constant = 6.67408E-11
+G = gravitational_constant
 """Гравитационная постоянная Ньютона G"""
 
 
@@ -14,8 +15,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx += G*body.m*obj.m/((body.x - obj.x)**2
+        body.Fy += G*body.m*obj.m/((body.y - obj.y)**2
 
 
 def move_space_object(body, dt):
@@ -25,9 +26,12 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += body.Vx*dt
     body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+
+    ay = body.Fy / body.m
+    body.y += body.Vx*dt
+    body.Vy += ay * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
